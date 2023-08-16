6451e61b499e892d4a2d34aab71e51f67eb6e4e3
nkosyrev <shutnik1986@gmail.com>
2023-08-16 15:14:31 +0900

    Game is ready!

--- Saper.cpp
+++ Saper.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <thread>
+#include <chrono>
+
 
 
 using namespace std;
@@ -10,7 +14,8 @@ int field_p[100][100];
 // -2 - closed
 // -1 - flag
 // 0-8 - opened
-const int N = 5, M = 5, K = 5;
+const int N = 9, M = 9, K = 7;
+bool is_bot;
 
 void print_field() {
 	system("cls");
@@ -76,7 +81,30 @@ void end_game(bool is_win = false) {
 	cout << "Vi " << (is_win ? "viigrali" : "proigrali") << ", \nchtoby nachat novuyu igru vvedite lubuyu stroku:\n";
 	string s;
 	cin >> s;
+	is_bot = false;
 }
+
+void save_field() {
+	ofstream fout("field.txt", ios_base::trunc);
+	if (!fout.is_open()) {
+		cout << "No file field.txt";
+		exit(0);
+	}
+	fout << N << ' ' << M << ' ' << K << '\n';
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			fout << field_p[i][j] << ' ';
+		}
+		fout << '\n';
+	}
+	fout.close();
+}
+
+void wait(int sec = 2) {
+	this_thread::sleep_for(chrono::milliseconds(sec * 1000));
+}
+
+
 
 int main()
 {
@@ -122,11 +150,40 @@ int main()
 			\b
 			*/
 			string comand;
-			cin >> comand;
+			ifstream fin;
+			if (is_bot) {
+				save_field();
+				system("Bot.exe");
+				fin.open("comand.txt");
+				if (!fin.is_open()) {
+					int tries = 10;
+					int zaderzh = 1;
+					while (!fin.is_open() && tries--) {
+						cout << "Retry to find comand.txt file. " << tries << endl;
+						wait(zaderzh);
+						fin.open("comand.txt");
+					}
+					if (tries == -1) {
+						throw "No file comand.txt";
+					}
+				}
+				fin >> comand;
+				cout << comand << ' ';
+			}
+			else {
+				cin >> comand;
+			}
 			if (comand == "\\o") {
 				// vvod koordinat
 				int x, y;
-				cin >> x >> y;
+				if (is_bot) {
+					fin >> x >> y;
+					cout << x << ' ' << y;
+					wait();
+				}
+				else {
+					cin >> x >> y;
+				}
 				--x; --y;
 				// otkroem kletki
 				if (x < 0 || x >= N || y < 0 || y >= M || field_p[x][y] >= 0) continue;
@@ -142,7 +199,14 @@ int main()
 			else if (comand == "\\f") {
 				// vvod koordinat
 				int x, y;
-				cin >> x >> y;
+				if (is_bot) {
+					fin >> x >> y;
+					cout << x << ' ' << y;
+					wait();
+				}
+				else {
+					cin >> x >> y;
+				}
 				--x; --y;
 				if (x < 0 || x >= N || y < 0 || y >= M || field_p[x][y] >= 0) continue;
 				// put flag
@@ -150,6 +214,10 @@ int main()
 				else field_p[x][y] = -1;
 			}
 			else if (comand == "\\n") {
+				break;
+			}
+			else if (comand == "\\b") {
+				is_bot = true;
 				break;
 			}
 			else if (comand == "\\q") {
